d8c3862b2534cff0d35ec9e398c0b7ba6d93dd27
Tatyana <vta278@mail.ru>
2023-07-11 23:46:30 +0300

    добавлена задача2 сумма в промежутке

---
+++ task9_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Verificate(int n, int m)
+{
+if(n < m) 
+{System.Console.WriteLine("число N больше M, введите другие числа");
+ return false;
+}
+return true;
+}
+
+int sum = 0;
+
+int SumNumber(int n, int m)
+{
+        sum = sum + (m);
+        if (m >= n) return sum;
+    return SumNumber(n, m + 1);
+}
+
+int n = ReadInt("Введите число N > ");
+int m = ReadInt("Введите число M > ");
+if (Verificate(n,m))
+System.Console.WriteLine ($"сумма чисел от {m} до {n} = {SumNumber (n, m)}");
